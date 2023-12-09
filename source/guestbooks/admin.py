682c0eaf0498bedab414b5d7f11945bbd51a2de3
Makuda <vip.link.96@mail.ru>
2023-12-09 17:31:27 +0500

    изменил и заново зарегистрировал в админку то что должно будет выводиться на экран

--- source/guestbooks/admin.py
+++ source/guestbooks/admin.py
@@ -3,4 +3,9 @@ from guestbooks.models import Guest_books
 
 @admin.register(Guest_books)
 class Guest_booksAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'status', 'name', 'email', 'description', 'start_date', 'end_date']
+    list_display_links = ['id', 'status', 'name', 'email']
+    list_filter = ['name']
+    search_fields = ['id', 'status', 'name']
+    fields = ['status', 'name', 'email', 'description', 'start_date', 'end_date']
+    readonly_fields = ['start_date', 'end_date']
